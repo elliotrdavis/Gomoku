840d721889f8088c4be43aa215297c75c01cef3f
broreece <broreece@gmail.com>
2021-02-16 17:31:57 +0000

    Comments

--- GomokuAgent/player.py
+++ GomokuAgent/player.py
@@ -6,15 +6,32 @@ from gomokuAgent import GomokuAgent
 
 class Player(GomokuAgent):
     def move(self, board):
+        # - For every location in the board (x coordinate and y coordinate)
         for x in range(self.BOARD_SIZE):
             for y in range(self.BOARD_SIZE):
+
+                # - Generates a tuple of the possible move
                 moveLoc = (x, y)
+
+                # - Checks if the move is legal on the current board
                 if legalMove(board, moveLoc):
+
+                    ''' Copies the board and sets the copy board move location to the player's id
+                    (Marks the player's move as the current location on the copy board) '''
                     copyBoard = copy.deepcopy(board)
                     copyBoard[moveLoc] = self.ID
+
+                    # - If the player has a winning move play that winning move
                     if winningTest(self.ID, copyBoard, self.X_IN_A_LINE):
                         return moveLoc
 
+                    ''' - If the other player has a winning move block that move, we do x in a line -2 first to
+                    check if they have a checkmate type move (Imagine that a player needs to get 5 in a row if
+                    they have 3 in a row with both sides open they can place on one either side to get the win)'''
+                    if winningTest(self.ID, copyBoard, self.X_IN_A_LINE - 2):
+                        return moveLoc
+
+        ''' - If no optimal move just take a random move '''
         while True:
             moveLoc = tuple(np.random.randint(self.BOARD_SIZE, size=2))
             if legalMove(board, moveLoc):
