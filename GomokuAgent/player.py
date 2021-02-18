d3a55aa9c1197063e1f18c781b40a3d2bcbecd52
broreece <broreece@gmail.com>
2021-02-18 14:07:51 +0000

    This should function

--- GomokuAgent/player.py
+++ GomokuAgent/player.py
@@ -1,11 +1,14 @@
 import numpy as np
 import copy
 
-from misc import legalMove, winningTest
+from misc import legalMove, winningTest, diagTest, rowTest
 from gomokuAgent import GomokuAgent
 
 class Player(GomokuAgent):
     def move(self, board):
+        rankedMove = 0
+        move = (0, 0)
+
         # - For every location in the board (x coordinate and y coordinate)
         for x in range(self.BOARD_SIZE):
             for y in range(self.BOARD_SIZE):
@@ -25,21 +28,31 @@ class Player(GomokuAgent):
                     if winningTest(self.ID, copyBoard, self.X_IN_A_LINE):
                         return moveLoc
 
+
+
                     ''' - If the other player has a winning move block that move, we do x in a line -2 first to
                     check if they have a checkmate type move (Imagine that a player needs to get 5 in a row if
                     they have 3 in a row with both sides open they can place on one either side to get the win)'''
                     copyBoard[moveLoc] = self.ID * -1
 
+
+
                     if winningTest(self.ID, copyBoard, self.X_IN_A_LINE):
-                        return moveLoc
+                        rankedMove = 4
+                        move = (x, y)
 
-                    if winningTest(self.ID, copyBoard, self.X_IN_A_LINE - 1):
-                        return moveLoc
+                    if winningTest(self.ID, copyBoard, self.X_IN_A_LINE - 1 and rankedMove < 2):
+                        rankedMove = 2
+                        move = (x, y)
 
-                    if winningTest(self.ID, copyBoard, self.X_IN_A_LINE - 2):
-                        return moveLoc
+                    if winningTest(self.ID, copyBoard, self.X_IN_A_LINE - 2 and rankedMove < 1):
+                        rankedMove = 1
+                        move = (x, y)
 
         ''' - If no optimal move just take a random move '''
+        if rankedMove > 0:
+            return move
+
         while True:
             moveLoc = tuple(np.random.randint(self.BOARD_SIZE, size=2))
             if legalMove(board, moveLoc):
