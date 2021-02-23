b7c77e11e802b10e644de93bfbb58ebb2eb283bb
broreece <broreece@gmail.com>
2021-02-23 15:51:44 +0000

    Reverted Gomoku.py back to normal

--- gomoku.py
+++ gomoku.py
@@ -1,43 +1,48 @@
 #######################################################
 # Gomoku Platform (single game)
 # Version 0.5
-# 
+#
 # Xiuyi Fan, Matt Bastiman, Edward Wall, Joe Panes
 # Swansea University
 # Feb 2020
 #
 
-import sys, signal
+import sys, time, signal
 import numpy as np
 import os
 
 import concurrent.futures
 
+from time import time
 from random import randint
 
 from misc import winningTest, legalMove
 
-BOARD_SIZE = 11   # size of the board is 11-by-11
-X_IN_A_LINE = 5   # play the standard game with 5 stones in a line
-TIME_OUT = 5      # player must return a move within 5 seconds
+BOARD_SIZE = 11  # size of the board is 11-by-11
+X_IN_A_LINE = 5  # play the standard game with 5 stones in a line
+TIME_OUT = 5  # player must return a move within 5 seconds
+
 
 # an empty class to host the time-out exception
 class TimeOutException(Exception):
     pass
+
 
 # handler for time out
 def handler(signum, frame):
     print("Player timeout - signal -")
     raise TimeOutException()
 
+
 """
 CHANGE:
 Added turn_id to the turn function to replace the player.ID calls
 This protects against ID spoofing
 """
+
+
 # turn taking function
 def turn(board, player, turn_id):
-    
     # make a copy of the board, which is passed to the agent
     tempBoard = np.array(board)
 
@@ -45,21 +50,21 @@ def turn(board, player, turn_id):
     try:
         with concurrent.futures.ThreadPoolExecutor() as executor:
             playerThread = executor.submit(player.move, tempBoard)
-            moveLoc = playerThread.result(TIME_OUT+1)
-    except TimeOutException:        
+            moveLoc = playerThread.result(TIME_OUT + 1)
+    except TimeOutException:
         # Caught the signal timeout exception
         print("before pass")
         pass
     except concurrent.futures.TimeoutError:
         print("Player" + str(turn_id) + " time out.")
-        return turn_id*1, board        
+        return turn_id * 1, board
 
-    # test if the move is legal - on the original board
+        # test if the move is legal - on the original board
     if legalMove(board, moveLoc):
         board[moveLoc] = player.ID
     else:
         print("Player " + str(player.ID) + " illegal move at " + str(moveLoc))
-        return turn_id*-1, board
+        return turn_id * -1, board
 
     # test if any player wins the game
     if winningTest(player.ID, board, X_IN_A_LINE):
@@ -67,6 +72,7 @@ def turn(board, player, turn_id):
 
     # move to the next turn
     return 0, board
+
 
 def main():
     if len(sys.argv) < 3:
@@ -76,7 +82,7 @@ def main():
 
     # two directory names
     p1Dir, p2Dir = sys.argv[1], sys.argv[2]
-    
+
     # creating the two players
     P1 = getattr(__import__(p1Dir, fromlist=["player"]), "player")
     P2 = getattr(__import__(p2Dir, fromlist=["player"]), "player")
@@ -117,7 +123,8 @@ def main():
                 end = True
                 break
         if end:
-            break        
+            break
+
 
 if __name__ == '__main__':
     sys.exit(main());
