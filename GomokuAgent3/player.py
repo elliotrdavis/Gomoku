7f71159b6264eadf84042704a06182eb7c2e400b
elliotrdavis <elliotrdav@gmail.com>
2021-03-02 14:55:42 +0000

    code added tomaxValue and minValue

--- GomokuAgent3/player.py
+++ GomokuAgent3/player.py
@@ -1,16 +1,26 @@
 import numpy as np
+import copy
 
-from misc import legalMove
+from misc import legalMove, winningTest, diagTest, rowTest
 from gomokuAgent import GomokuAgent
 
 def minimaxDecision(state):
     return state
 
 def maxValue(state):
+    if winningTest(ID, board, X_IN_A_LINE):
+        move = bestMove(ID, board, X_IN_A_LINE)
+        return move
+    v = -np.inf
     return v
 
 def minValue(state):
-    retrun v
+    if winningTest(ID, board, X_IN_A_LINE):
+        move = bestMove(ID, board, X_IN_A_LINE)
+        return move
+    v = np.inf
+    #for a, s in SUCCESSORS(state) do v<-min(v, max-value)
+    return v
 
 class Player(GomokuAgent):
     def move(self, board):
